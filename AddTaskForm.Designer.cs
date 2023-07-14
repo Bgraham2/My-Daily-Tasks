c59797c1392d24e74597607205aa9dc80420f1ba
BrianGraham <bgraham2@gmail.com>
2023-07-13 19:46:01 -0500

    Add project files.

---
+++ AddTaskForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace My_Daily_Tasks
+{
+    partial class AddTaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTaskName = new System.Windows.Forms.Label();
+            this.textBoxTaskName = new System.Windows.Forms.TextBox();
+            this.checkBoxAllWeek = new System.Windows.Forms.CheckBox();
+            this.groupBoxDays = new System.Windows.Forms.GroupBox();
+            this.checkBoxMonday = new System.Windows.Forms.CheckBox();
+            this.checkBoxTuesday = new System.Windows.Forms.CheckBox();
+            this.checkBoxWednesday = new System.Windows.Forms.CheckBox();
+            this.checkBoxThursday = new System.Windows.Forms.CheckBox();
+            this.checkBoxFriday = new System.Windows.Forms.CheckBox();
+            this.checkBoxSaturday = new System.Windows.Forms.CheckBox();
+            this.checkBoxSunday = new System.Windows.Forms.CheckBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.groupBoxDays.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTaskName
+            // 
+            this.labelTaskName.AutoSize = true;
+            this.labelTaskName.Location = new System.Drawing.Point(12, 9);
+            this.labelTaskName.Name = "labelTaskName";
+            this.labelTaskName.Size = new System.Drawing.Size(65, 13);
+            this.labelTaskName.TabIndex = 0;
+            this.labelTaskName.Text = "Task Name:";
+            // 
+            // textBoxTaskName
+            // 
+            this.textBoxTaskName.Location = new System.Drawing.Point(83, 6);
+            this.textBoxTaskName.Name = "textBoxTaskName";
+            this.textBoxTaskName.Size = new System.Drawing.Size(195, 20);
+            this.textBoxTaskName.TabIndex = 1;
+            // 
+            // checkBoxAllWeek
+            // 
+            this.checkBoxAllWeek.AutoSize = true;
+            this.checkBoxAllWeek.Location = new System.Drawing.Point(323, 9);
+            this.checkBoxAllWeek.Name = "checkBoxAllWeek";
+            this.checkBoxAllWeek.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxAllWeek.TabIndex = 2;
+            this.checkBoxAllWeek.Text = "All Week";
+            this.checkBoxAllWeek.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxDays
+            // 
+            this.groupBoxDays.Controls.Add(this.checkBoxSunday);
+            this.groupBoxDays.Controls.Add(this.checkBoxSaturday);
+            this.groupBoxDays.Controls.Add(this.checkBoxFriday);
+            this.groupBoxDays.Controls.Add(this.checkBoxThursday);
+            this.groupBoxDays.Controls.Add(this.checkBoxWednesday);
+            this.groupBoxDays.Controls.Add(this.checkBoxTuesday);
+            this.groupBoxDays.Controls.Add(this.checkBoxMonday);
+            this.groupBoxDays.Location = new System.Drawing.Point(12, 62);
+            this.groupBoxDays.Name = "groupBoxDays";
+            this.groupBoxDays.Size = new System.Drawing.Size(776, 100);
+            this.groupBoxDays.TabIndex = 3;
+            this.groupBoxDays.TabStop = false;
+            this.groupBoxDays.Text = "Week Days";
+            // 
+            // checkBoxMonday
+            // 
+            this.checkBoxMonday.AutoSize = true;
+            this.checkBoxMonday.Location = new System.Drawing.Point(6, 42);
+            this.checkBoxMonday.Name = "checkBoxMonday";
+            this.checkBoxMonday.Size = new System.Drawing.Size(64, 17);
+            this.checkBoxMonday.TabIndex = 0;
+            this.checkBoxMonday.Text = "Monday";
+            this.checkBoxMonday.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTuesday
+            // 
+            this.checkBoxTuesday.AutoSize = true;
+            this.checkBoxTuesday.Location = new System.Drawing.Point(76, 42);
+            this.checkBoxTuesday.Name = "checkBoxTuesday";
+            this.checkBoxTuesday.Size = new System.Drawing.Size(67, 17);
+            this.checkBoxTuesday.TabIndex = 1;
+            this.checkBoxTuesday.Text = "Tuesday";
+            this.checkBoxTuesday.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxWednesday
+            // 
+            this.checkBoxWednesday.AutoSize = true;
+            this.checkBoxWednesday.Location = new System.Drawing.Point(150, 42);
+            this.checkBoxWednesday.Name = "checkBoxWednesday";
+            this.checkBoxWednesday.Size = new System.Drawing.Size(83, 17);
+            this.checkBoxWednesday.TabIndex = 2;
+            this.checkBoxWednesday.Text = "Wednesday";
+            this.checkBoxWednesday.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxThursday
+            // 
+            this.checkBoxThursday.AutoSize = true;
+            this.checkBoxThursday.Location = new System.Drawing.Point(240, 42);
+            this.checkBoxThursday.Name = "checkBoxThursday";
+            this.checkBoxThursday.Size = new System.Drawing.Size(70, 17);
+            this.checkBoxThursday.TabIndex = 3;
+            this.checkBoxThursday.Text = "Thursday";
+            this.checkBoxThursday.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFriday
+            // 
+            this.checkBoxFriday.AutoSize = true;
+            this.checkBoxFriday.Location = new System.Drawing.Point(317, 42);
+            this.checkBoxFriday.Name = "checkBoxFriday";
+            this.checkBoxFriday.Size = new System.Drawing.Size(54, 17);
+            this.checkBoxFriday.TabIndex = 4;
+            this.checkBoxFriday.Text = "Friday";
+            this.checkBoxFriday.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSaturday
+            // 
+            this.checkBoxSaturday.AutoSize = true;
+            this.checkBoxSaturday.Location = new System.Drawing.Point(378, 42);
+            this.checkBoxSaturday.Name = "checkBoxSaturday";
+            this.checkBoxSaturday.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxSaturday.TabIndex = 5;
+            this.checkBoxSaturday.Text = "Saturday";
+            this.checkBoxSaturday.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSunday
+            // 
+            this.checkBoxSunday.AutoSize = true;
+            this.checkBoxSunday.Location = new System.Drawing.Point(453, 42);
+            this.checkBoxSunday.Name = "checkBoxSunday";
+            this.checkBoxSunday.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxSunday.TabIndex = 6;
+            this.checkBoxSunday.Text = "Sunday";
+            this.checkBoxSunday.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(713, 168);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(617, 168);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // AddTaskForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.groupBoxDays);
+            this.Controls.Add(this.checkBoxAllWeek);
+            this.Controls.Add(this.textBoxTaskName);
+            this.Controls.Add(this.labelTaskName);
+            this.Name = "AddTaskForm";
+            this.Text = "Add Task";
+            this.groupBoxDays.ResumeLayout(false);
+            this.groupBoxDays.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTaskName;
+        private System.Windows.Forms.TextBox textBoxTaskName;
+        private System.Windows.Forms.CheckBox checkBoxAllWeek;
+        private System.Windows.Forms.GroupBox groupBoxDays;
+        private System.Windows.Forms.CheckBox checkBoxSunday;
+        private System.Windows.Forms.CheckBox checkBoxSaturday;
+        private System.Windows.Forms.CheckBox checkBoxFriday;
+        private System.Windows.Forms.CheckBox checkBoxThursday;
+        private System.Windows.Forms.CheckBox checkBoxWednesday;
+        private System.Windows.Forms.CheckBox checkBoxTuesday;
+        private System.Windows.Forms.CheckBox checkBoxMonday;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
