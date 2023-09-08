f247c1923bbf2edbe8e3e82259370b0ecb41dfb7
BrianGraham <bgraham2@gmail.com>
2023-09-08 14:42:39 -0500

    Added completion form

--- MainForm.cs
+++ MainForm.cs
@@ -9,6 +9,7 @@ namespace My_Daily_Tasks
     {
         private readonly String today = DateTime.Today.DayOfWeek.ToString();
         private int taskCompleted = 0;
+        private TasksComplete complete = new TasksComplete();
 
         public MainForm()
         {
@@ -42,6 +43,7 @@ namespace My_Daily_Tasks
             {
                 dataGridViewTasks.Rows[e.RowIndex].Cells[2].Style.BackColor = Color.Green;
                 taskCompleted++;
+                complete.AllTasksComplete(dataGridViewTasks.Rows.Count, taskCompleted);
             }
 
             if (e.ColumnIndex == 1 && e.RowIndex >= 0)
---
+++ TasksComplete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Daily_Tasks
+{
+    class TasksComplete
+    {
+        public TasksComplete()
+        {
+
+        }
+
+        public void AllTasksComplete(int tasks, int tasksCompleted)
+        {
+            if (tasks == tasksCompleted)
+            {
+                AllTasksCompleted all = new AllTasksCompleted();
+                all.Show();
+            }
+        }
+    }
+}
