4fe0caf8f9e2dcb39a923dd1c3e393f2f510dbeb
BrianGraham <bgraham2@gmail.com>
2023-10-04 09:29:59 -0500

    Added contructor

--- NotificationHelper.cs
+++ NotificationHelper.cs
@@ -6,5 +6,11 @@ namespace My_Daily_Tasks
     class NotificationHelper
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public NotificationHelper()
+        {
+
+        }
+
     }
 }
