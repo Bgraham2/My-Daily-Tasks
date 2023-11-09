25d04dea0f5659d9143aab1fa0ed4ab72eed6319
BrianGraham <bgraham2@gmail.com>
2023-11-08 18:06:11 -0600

    Timers set

--- NotificationHelper.cs
+++ NotificationHelper.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Timers;
 
 namespace My_Daily_Tasks
 {
@@ -6,10 +6,30 @@ namespace My_Daily_Tasks
     class NotificationHelper
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private Timer notification = new Timer();
 
         public NotificationHelper()
         {
+            log.Info("Notifications called!");
+        }
 
+        public void StartNotifications()
+        {
+            notification.Elapsed += new ElapsedEventHandler(Notification);
+            notification.Interval = 3600000;
+            notification.Enabled = true;
+            log.Info("Notifications started.");
+        }
+
+        private void Notification(object source, ElapsedEventArgs e)
+        {
+            
+        }
+
+        public void StopNotifications()
+        {
+            notification.Enabled = false;
+            log.Info("Notifications stopped.");
         }
 
     }
