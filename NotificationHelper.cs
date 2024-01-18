0d7b5b4d0a118197a8529a0c00d4125f32180948
BrianGraham <bgraham2@gmail.com>
2024-01-18 11:53:23 -0600

    changed harcoded array to read file directory, now user can change songs

--- NotificationHelper.cs
+++ NotificationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Timers;
 
 namespace My_Daily_Tasks
@@ -7,15 +8,14 @@ namespace My_Daily_Tasks
     class NotificationHelper
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        private static readonly string[] songs = { "21 Pilots - Heathens.mp3", "Aerosmith - Eat the rich.mp3", "Avenge Sevenfold - Hail to the king.mp3", "Beatie Boys - Sabotage.mp3", 
-            "Cinderella - If You Don't Like It.mp3", "Disturbed - Indestructible.mp3", "Dokken - Mr Scary.mp3", "Led Zeppelin - Immigrant Song.mp3", "Megadeth - Symphony Of Destruction.mp3",
-            "Rage Against The Machine - Killing In the Name.mp3", "Rage Against The Machine - Testify.mp3", "Rammstein - Feuer Frei!.mp3", "Ratt - Way Cool Jr..mp3", "Slade - Run Run Away.mp3",
-            "Savatage - Hall of the Mountain King.mp3", "The Cult - Fire Woman.mp3"};
+        private static readonly string path = "Songs\\";
+        private static readonly string[] songs = Directory.GetFiles(path);
+        private readonly Random random = new Random();
         private readonly Timer notification = new Timer();
 
         public NotificationHelper()
         {
-            log.Info("Notifications called!");
+            log.Info("Notifications called! " + String.Join(", ", songs));
         }
 
         public void StartNotifications()
@@ -28,13 +28,11 @@ namespace My_Daily_Tasks
 
         private void Notification(object source, ElapsedEventArgs e)
         {
-            Random random = new Random();
-            int index = random.Next(songs.Length);
-            string song = songs[index];
-            log.Info("Notification playing");
+     
+            log.Info("Notification playing" + songs[random.Next(songs.Length)]);
             WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer
             {
-                URL = "Songs\\" + song
+                URL = songs[random.Next(songs.Length)]
             };
             player.controls.play();
         }
