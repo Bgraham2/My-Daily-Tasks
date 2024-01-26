b3d0ede0fa96960a3a429fdf1af235bd16c74c8d
BrianGraham <bgraham2@gmail.com>
2024-01-26 12:25:15 -0600

    Updated Notification form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,19 +29,60 @@ namespace My_Daily_Tasks
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelSongTitle = new System.Windows.Forms.Label();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.buttonStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
             this.SuspendLayout();
+            // 
+            // labelSongTitle
+            // 
+            this.labelSongTitle.AutoSize = true;
+            this.labelSongTitle.Font = new System.Drawing.Font("Lucida Handwriting", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSongTitle.Location = new System.Drawing.Point(291, 9);
+            this.labelSongTitle.Name = "labelSongTitle";
+            this.labelSongTitle.Size = new System.Drawing.Size(130, 27);
+            this.labelSongTitle.TabIndex = 0;
+            this.labelSongTitle.Text = "Song Title";
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.Location = new System.Drawing.Point(260, 61);
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(201, 45);
+            this.trackBarVolume.TabIndex = 1;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(325, 125);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.trackBarVolume);
+            this.Controls.Add(this.labelSongTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Notifications";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelSongTitle;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.Button buttonStop;
     }
 }
